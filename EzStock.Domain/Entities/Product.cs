f3d471d9aa51e526aa936579c53fda38d17e925d
Teo Giampietro <tgiampietro@fyo.com>
2023-01-18 14:39:02 -0300

    Add project files.

---
+++ EzStock.Domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EzStock.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int IdProduct { get; set; }
+        [Required]
+        public string? Description { get; set; }
+    }
+}
---
+++ EzStock.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EzStock.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EzStock.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        DbSet<Product> Products { get; set; }
+    }
+}
