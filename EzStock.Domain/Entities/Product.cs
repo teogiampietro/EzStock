2a1dca49aba348e4715b36d00baf91a0001ee403
Teo Giampietro <teogiampietro@hotmail.com>
2023-01-20 11:15:45 -0300

    Create product

--- EzStock.Domain/Entities/Product.cs
+++ EzStock.Domain/Entities/Product.cs
@@ -5,7 +5,6 @@ namespace EzStock.Domain.Entities
     public class Product
     {
         [Key]
-        [Required]
         public int IdProduct { get; set; }
         [Required]
         public string? Description { get; set; }
