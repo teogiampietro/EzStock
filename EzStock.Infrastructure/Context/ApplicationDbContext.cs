42e741d6613aa218468edc367f7f3f9930a22c58
Teo Giampietro <teogiampietro@hotmail.com>
2023-02-28 21:55:42 -0300

    Removed commented code

--- EzStock.Infrastructure/Context/ApplicationDbContext.cs
+++ EzStock.Infrastructure/Context/ApplicationDbContext.cs
@@ -10,46 +10,6 @@ namespace EzStock.Infrastructure.Context
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-        public DbSet<Product> Products { get; set; }
-        //protected override void OnModelCreating(ModelBuilder builder)
-        //{
-        //    base.OnModelCreating(builder);
-        //
-        //    string ADMIN_ID = "cac43a6e-f7bb-4448-baaf-1add431ccbbf";
-        //    string ROLE_ID = "b8633e2d-a33b-45e6-8329-1958b3252bbd";
-        //
-        //    //seed admin role
-        //    builder.Entity<IdentityRole>().HasData(new IdentityRole
-        //    {
-        //        Name = "SuperAdmin",
-        //        NormalizedName = "SuperAdmin".ToUpper(),
-        //        Id = ROLE_ID,
-        //        ConcurrencyStamp = ROLE_ID
-        //    });
-        //
-        //    //create user
-        //    var appUser = new IdentityUser
-        //    {
-        //        Id = ADMIN_ID,
-        //        Email = "superAdmin@gmail.com",
-        //        EmailConfirmed = true,
-        //        UserName = "admin",
-        //        NormalizedUserName = "admin".ToUpper()
-        //    };
-        //
-        //    //set user password
-        //    PasswordHasher<IdentityUser> ph = new PasswordHasher<IdentityUser>();
-        //    appUser.PasswordHash = ph.HashPassword(appUser, "asdqwe123");
-        //
-        //    //seed user
-        //    builder.Entity<IdentityUser>().HasData(appUser);
-        //
-        //    //set user role to admin
-        //    builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
-        //    {
-        //        RoleId = ROLE_ID,
-        //        UserId = ADMIN_ID
-        //    });
-        //}
+        public DbSet<Product> Products { get; set; }      
     }
 }
